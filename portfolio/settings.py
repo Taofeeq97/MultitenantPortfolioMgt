7d3a321076f03240a81e948d116de84d332db73b
Your Name <otutaofeeqi@gmail.com>
2023-08-13 06:25:06 +0100

    db issues

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',    'django.contrib.staticfiles',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'client',
     
@@ -184,7 +185,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT= os.path.join(BASE_DIR,'static/images')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
