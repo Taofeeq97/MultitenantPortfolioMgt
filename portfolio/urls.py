166801f875d23ab5775c07def8f819ff20f76220
Your Name <otutaofeeqi@gmail.com>
2023-08-13 19:28:21 +0100

    swagger

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from rest_framework_swagger.views import get_swagger_view
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
